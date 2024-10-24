24c5afffebfcbcf25339eff83b71ff747fee29fa
Mehrnaz <mehrnaz.nemati2000@gmail.com>
2024-10-24 13:09:06 -0400

    first day exercise completion

---
+++ final_day_1_project.py
@@ -0,0 +1,8 @@
+#print greeting message
+print("Welcome to the Band Name Generator.")
+#ask about the city you born
+city = input("What's the name of the city you grew up in?\n")
+#ask about the pet's name
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+
