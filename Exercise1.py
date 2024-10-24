97271e6b9ff2432dcfd2a0fb6fe5f64f379fe7c1
Mehrnaz <mehrnaz.nemati2000@gmail.com>
2024-10-24 12:25:51 -0400

    removed the duplicate line

--- Exercise1.py
+++ Exercise1.py
@@ -3,4 +3,4 @@ print("2. Knead the dough for 10 minutes.")
 print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
 print("5. Bake at 200 degrees C for 30 minutes.")
-print("5. Bake at 200 degrees C for 30 minutes.")
+
